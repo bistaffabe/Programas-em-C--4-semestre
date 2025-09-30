4461e9331071df57cd30b416e12627a36cad0f75
Beatriz Bistaffa Vitor <166538919+bistaffabe@users.noreply.github.com>
2025-09-30 20:56:19 -0300

    Update Projeto1.c

--- Projeto1/Projeto1.c
+++ Projeto1/Projeto1.c
@@ -4,27 +4,28 @@
 #include <locale.h>
 #include "biblioteca_projeto1.h"
 #include <ctype.h>
+//#include <conio.h>
+#include<time.h>//necessário p/ função time()
 
 #include <unistd.h>
 
 int main ()
 {
-    setlocale(LC_ALL, "Portuguese");
+    setlocale(LC_ALL, "portuguese");
     Fila* emerg;
     Fila* normal;
     Fila* atendidos;
     Fila* geral;
+    Dados A;
     emerg = CriaFila();
     normal = CriaFila();
     atendidos = CriaFila();
     geral = CriaFila();
-    int num, id, idade, prior, dia, mes, ano, busca_resul;
+    int num, id, idade, prior, dia, mes, ano, busca_resul,b, buscado1;
     char nome[50], buscado[50];
     char especie[30];
 
-    do {
-        do {
-               sleep(1);
+            printf("\n");
             printf("          /^ ^\\\n");
             printf("         / 0 0 \\\n");
             printf("         V\\ Y /V\n");
@@ -33,6 +34,9 @@ int main ()
             printf("        V__) (__V\n");
             printf("\n");
 
+    do {
+        do {
+
 
             printf("\n\n-----------------------------------------------------\n");
             printf("\t\t\tMENU\n");
@@ -40,87 +44,88 @@ int main ()
             printf("1. Insira um pet na fila de atendimento.\n\n");
             printf("2. Atender um pet.\n\n");
             printf("3. Buscar um pet.\n\n");
-            printf("4. Imprimir relat�rio dos pets na fila.\n\n");
-            printf("5. Imprimir pr�ximo pet a ser atendido. \n\n");
-            printf("6. Imprimir todos os pets que j� foram atendidos. \n\n");
+            printf("4. Imprimir relatorio dos pets na fila.\n\n");
+            printf("5. Imprimir proximo pet a ser atendido. \n\n");
+            printf("6. Imprimir todos os pets que ja foram atendidos. \n\n");
             printf("7. Finalizar o sistema. \n\n");
             printf("-----------------------------------------------------\n\n");
-            printf("Insira sua op��o: ");
+            printf("Insira sua opcao: ");
             scanf("%d", &num);
             //system("cls");
         } while (num<1 || num>7);
         switch (num) {
             case 1:
-
+                // Dados A;
                 printf("-----------------------------------------------------");
                 printf("\n\t\tFicha de cadastro\n");
                 printf("-----------------------------------------------------");
+
+                id = gera();
+                printf("\nID do pet: %d", A.id);
+
                 printf("\nNome do pet: ");
-                scanf("%s", nome);
-
-                //printf("\nID do pet: ");
-                // scanf("%d", &id);
-
+                scanf("%s", A.nome);
 
                 printf("\nIdade do pet: ");
-                scanf("%d", &idade);
+                scanf("%d", &A.idade);
+
                 while (idade < 0 || idade > 25)
                 {
-                    printf("Valor invalido, digite novamente: ");
-                    scanf("%d", &idade);
+                    printf("Valor inválido, digite novamente: ");
+                    scanf("%d", &A.idade);
                 }
 
-                printf("\nEsp�cie do pet: ");
-                scanf("%s", especie);
+                printf("\nEspecie do pet: ");
+                scanf("%s", A.especie);
 
                 printf("\nData de nascimento: ");
                 printf("\n Dia: ");
-                scanf("%d", &dia);
-                while (dia < 1 || dia > 31)
+                scanf("%d", &A.aniv.dia);
+                while (A.aniv.dia < 1 || A.aniv.dia > 31)
                 {
                     printf(" Valor invalido, digite novamente: ");
-                    scanf("%d", &dia);
+                    scanf("%d", &A.aniv.dia);
                 }
-                printf(" M�s: ");
-                scanf("%d", &mes);
-                while (mes < 1 || mes > 12)
+                printf(" Mês: ");
+                scanf("%d", &A.aniv.mes);
+                while (A.aniv.mes < 1 || A.aniv.mes > 12)
                 {
                     printf(" Valor invalido, digite novamente: ");
-                    scanf("%d", &mes);
+                    scanf("%d", &A.aniv.mes);
                 }
 
                 printf(" Ano: ");
-                scanf("%d", &ano);
-                while (ano < 2000 || ano > 2025)
+                scanf("%d", &A.aniv.ano);
+                while (A.aniv.ano < 2000 || A.aniv.ano > 2025)
                 {
                     printf(" Valor invalido, digite novamente: ");
-                    scanf("%d", &ano);
+                    scanf("%d", &A.aniv.ano);
                 }
 
-                printf("\nPrioridade do pet [0: Emerg�ncia | 1: Normal]: ");
-                scanf("%d", &prior);
+                printf("\nPrioridade do pet [0: Emergencia | 1: Normal]: ");
+                scanf("%d", &A.prior);
 
 
-                if (prior==0)
+                if (A.prior==0)
                 {
-                    insere_pet(emerg, nome, idade, especie, prior, dia, mes, ano);
+                    insere_pet(emerg, A);
                 }
                 else
                 {
-                    insere_pet(normal, nome, idade, especie, prior, dia, mes, ano);
+                    insere_pet(normal, A);
                 }
-                    insere_pet(geral, nome, idade, especie, prior, dia, mes, ano);
-                //system("cls");
+                    insere_pet(geral, A);
+
                 break;
 
             case 2:
                 if (vaziaFila(emerg)==1)
                 {
-                    atende(normal, atendidos);
+                    atende(normal, atendidos, geral);
                 }
                 else
                 {
-                    atende(emerg, atendidos);
+                    atende(emerg, atendidos, geral);
                 }
                 printf("\n------------------------------------------------------");
                 printf("\n\t\tPet Atendido\n");
@@ -128,19 +133,33 @@ int main ()
                 break;
 
             case 3:
+                printf("\nComo deseja buscar? (1 - ID / 2 - Nome)");
+                scanf("%d", &b);
+                if(b==1)
+                {
+                printf("\nID do animal que deseja buscar: ");
+                scanf("%d", buscado1);
+                busca_resul = buscaid(geral, buscado1);
+                }
+                else
+                {
                 printf("\nNome do animal que deseja buscar: ");
                 scanf("%s", buscado);
-                busca_resul = busca(geral, buscado);
+                busca_resul = buscanome(geral, buscado);
+                }
+
                 if (busca_resul == 0)
                 {
-                    printf("\nO animal procurado n�o est� cadastrado.");
+                    printf("\nO animal procurado não está cadastrado.");
                 }
+
 
                 break;
 
             case 4:
-                printf("Emerg�ncia: ");
+                printf("Emergencia: ");
                 imprime(emerg);
+                printf("\n");
                 printf("\nAtendimento normal: ");
                 imprime(normal);
                 /*fflush(stdin);
